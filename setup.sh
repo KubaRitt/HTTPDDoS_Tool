2ccb39da21cd17089c10ca5d31f11ed19aedb272
KubaRitt <144609494+KubaRitt@users.noreply.github.com>
2024-01-16 15:36:34 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 clear
+echo "Example usage: python DDoS.py www.Website.com 200 30
+                                                    |    |
+                                                    |    |
+                                                  Timer Threads"
 apt-get -y install python-pip > /dev/null 2>&1
 apt-get -y install python3-pip > /dev/null 2>&1
 python3 -m pip install --upgrade pip > /dev/null 2>&1
